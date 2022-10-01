dc598d28ceaa07dd6e9f63ee9bbf8dab1da3738b
Greyden <vadercat1@icloud.com>
2022-10-01 14:19:11 -0500

    Began level 1 and 3 scenarios

--- functions.py
+++ functions.py
@@ -17,3 +17,32 @@ def clear():
 		os.system("cls")
 	elif sys.platform.startswith("darwin"):
 		os.system("clear")
+
+def radiation_scenario(stats):
+	typing("Over the time you've been in space thus far, \
+		you've been exposed to enough radiation to notice bodily changes.")
+	if stats["radiation"] < 5:
+		typing("Your health is worsening.") #maybe add symptoms of radiation poisoning
+	elif stats["radiation"] >= 5:
+		typing("You manage to ")
+	clear()
+
+def enrichment_scenario1():
+	clear()
+	typing("You have been in your capsule for months now, and you begin to realize that your personal space is being invaded from the small, tight space.")
+	
+
+def enrichment_scenario2():
+	clear()
+
+def isolation_scenario1():
+	clear()
+
+def isolation_scenario2():
+	clear()
+
+def starvation_scenario1():
+	clear()
+
+def starvation_scenario2():
+	clear()
